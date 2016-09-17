c74b9c677c6f14bb443ad433122cc071482d8bc0
alafr <antlafranchis@gmail.com>
2016-09-17 18:56:19 +0300

    Html entities
    Fix issue #6

--- source.js
+++ source.js
@@ -73,7 +73,7 @@ var ParseXml = function(XmlString) { // Convert a XML string into an object simu
       return(GetElementById(this, id) || null);
   };
   // Code adapted from this one: https://github.com/segmentio/xml-parser/
-  XmlString = XmlString.replace(/<!--[\s\S]*?-->/g, '').trim(); // Remove comments
+  XmlString = XmlString.replace(/<!--[\s\S]*?-->/g, '').replace(/<![\s\S]*?>/g, '').trim(); // Remove comments
   return(RecursiveParse());
   function RecursiveParse() {
     var temp, child, node, attr, value;
@@ -98,7 +98,7 @@ var ParseXml = function(XmlString) { // Convert a XML string into an object simu
       return(node);
     } else if (temp = ConsumeMatch(/^([^<]+)/)) { // Text node
       node = new SvgNode('#text');
-      node.nodeValue = temp[1];
+      node.nodeValue = DecodeHtmlEntities(temp[1]);
       return(node);
     }
   }
@@ -108,7 +108,23 @@ var ParseXml = function(XmlString) { // Convert a XML string into an object simu
     XmlString = XmlString.slice(temp[0].length);
     return(temp);
   }
-}
+  function DecodeHtmlEntities(Str) {
+    var Entities = {quot: 34, amp: 38, lt: 60, gt: 62, apos: 39, OElig: 338, oelig: 339, Scaron: 352, scaron: 353, Yuml: 376, circ: 710, tilde: 732, ensp: 8194, emsp: 8195, thinsp: 8201, zwnj: 8204, zwj: 8205, lrm: 8206, rlm: 8207, ndash: 8211, mdash: 8212, lsquo: 8216, rsquo: 8217, sbquo: 8218, ldquo: 8220, rdquo: 8221, bdquo: 8222, dagger: 8224, Dagger: 8225, permil: 8240, lsaquo: 8249, 
+         rsaquo: 8250, euro: 8364, nbsp: 160, iexcl: 161, cent: 162, pound: 163, curren: 164, yen: 165, brvbar: 166, sect: 167, uml: 168, copy: 169, ordf: 170, laquo: 171, not: 172, shy: 173, reg: 174, macr: 175, deg: 176, plusmn: 177, sup2: 178, sup3: 179, acute: 180, micro: 181, para: 182, middot: 183, cedil: 184, sup1: 185, ordm: 186, raquo: 187, frac14: 188, frac12: 189, frac34: 190, 
+         iquest: 191, Agrave: 192, Aacute: 193, Acirc: 194, Atilde: 195, Auml: 196, Aring: 197, AElig: 198, Ccedil: 199, Egrave: 200, Eacute: 201, Ecirc: 202, Euml: 203, Igrave: 204, Iacute: 205, Icirc: 206, Iuml: 207, ETH: 208, Ntilde: 209, Ograve: 210, Oacute: 211, Ocirc: 212, Otilde: 213, Ouml: 214, times: 215, Oslash: 216, Ugrave: 217, Uacute: 218, Ucirc: 219, Uuml: 220, Yacute: 221, 
+         THORN: 222, szlig: 223, agrave: 224, aacute: 225, acirc: 226, atilde: 227, auml: 228, aring: 229, aelig: 230, ccedil: 231, egrave: 232, eacute: 233, ecirc: 234, euml: 235, igrave: 236, iacute: 237, icirc: 238, iuml: 239, eth: 240, ntilde: 241, ograve: 242, oacute: 243, ocirc: 244, otilde: 245, ouml: 246, divide: 247, oslash: 248, ugrave: 249, uacute: 250, ucirc: 251, uuml: 252, 
+         yacute: 253, thorn: 254, yuml: 255, fnof: 402, Alpha: 913, Beta: 914, Gamma: 915, Delta: 916, Epsilon: 917, Zeta: 918, Eta: 919, Theta: 920, Iota: 921, Kappa: 922, Lambda: 923, Mu: 924, Nu: 925, Xi: 926, Omicron: 927, Pi: 928, Rho: 929, Sigma: 931, Tau: 932, Upsilon: 933, Phi: 934, Chi: 935, Psi: 936, Omega: 937, alpha: 945, beta: 946, gamma: 947, delta: 948, epsilon: 949, 
+         zeta: 950, eta: 951, theta: 952, iota: 953, kappa: 954, lambda: 955, mu: 956, nu: 957, xi: 958, omicron: 959, pi: 960, rho: 961, sigmaf: 962, sigma: 963, tau: 964, upsilon: 965, phi: 966, chi: 967, psi: 968, omega: 969, thetasym: 977, upsih: 978, piv: 982, bull: 8226, hellip: 8230, prime: 8242, Prime: 8243, oline: 8254, frasl: 8260, weierp: 8472, image: 8465, real: 8476, 
+         trade: 8482, alefsym: 8501, larr: 8592, uarr: 8593, rarr: 8594, darr: 8595, harr: 8596, crarr: 8629, lArr: 8656, uArr: 8657, rArr: 8658, dArr: 8659, hArr: 8660, forall: 8704, part: 8706, exist: 8707, empty: 8709, nabla: 8711, isin: 8712, notin: 8713, ni: 8715, prod: 8719, sum: 8721, minus: 8722, lowast: 8727, radic: 8730, prop: 8733, infin: 8734, ang: 8736, and: 8743, or: 8744, 
+         cap: 8745, cup: 8746, int: 8747, there4: 8756, sim: 8764, cong: 8773, asymp: 8776, ne: 8800, equiv: 8801, le: 8804, ge: 8805, sub: 8834, sup: 8835, nsub: 8836, sube: 8838, supe: 8839, oplus: 8853, otimes: 8855, perp: 8869, sdot: 8901, lceil: 8968, rceil: 8969, lfloor: 8970, rfloor: 8971, lang: 9001, rang: 9002, loz: 9674, spades: 9824, clubs: 9827, hearts: 9829, diams: 9830};
+    return(Str.replace(/&(?:#([0-9]+)|#[xX]([0-9A-Fa-f]+)|([0-9A-Za-z]+));/g, function(mt, m0, m1, m2) {
+      console.log(mt, m0,m1,m2)
+      if (m0) {return(String.fromCharCode(parseInt(m0, 10)));}
+      else if (m1) {return(String.fromCharCode(parseInt(m1, 16)));}
+      else {return(String.fromCharCode(Entities[m2]) || mt);}
+    }));
+  }
+};
 
 var SVGtoPDF = function(doc, svg, x, y, options) {
 
@@ -129,13 +145,18 @@ var SVGtoPDF = function(doc, svg, x, y, options) {
         return([255,255,255,0]);
       } else if (Temp = NamedColors[v]) {
         return(Temp.concat(1));
-      } else if (Temp = v.match(/^rgba\(([0-9]+),\s*([0-9]+),\s*([0-9]+),\s*([0-9.]+)\)$/)) {
+      } else if (Temp = v.match(/^rgba\(\s*([0-9]+)\s*,\s*([0-9]+)\s*,\s*([0-9]+)\s*,\s*([0-9.]+)\s*\)$/)) {
         Temp[1] = parseInt(Temp[1]); Temp[2] = parseInt(Temp[2]); Temp[3] = parseInt(Temp[3]); Temp[4] = parseFloat(Temp[4]);
         if (Temp[1] < 256 && Temp[2] < 256 && Temp[3] < 256 && Temp[4] <= 1) {
           return(Temp.slice(1, 5));
         }
-      } else if (Temp = v.match(/^rgb\(([0-9]+),\s*([0-9]+),\s*([0-9]+)\)$/)) {
+      } else if (Temp = v.match(/^rgb\(\s*([0-9]+)\s*,\s*([0-9]+)\s*,\s*([0-9]+)\s*\)$/)) {
         Temp[1] = parseInt(Temp[1]); Temp[2] = parseInt(Temp[2]); Temp[3] = parseInt(Temp[3]);
+        if (Temp[1] < 256 && Temp[2] < 256 && Temp[3] < 256) {
+          return(Temp.slice(1, 4).concat(1));
+        }
+      } else if (Temp = v.match(/^rgb\(\s*([0-9.]+)%\s*,\s*([0-9.]+)%\s*,\s*([0-9.]+)%\s*\)$/)) {
+        Temp[1] = 2.55 * parseFloat(Temp[1]); Temp[2] = 2.55 * parseFloat(Temp[2]); Temp[3] = 2.55 * parseFloat(Temp[3]);
         if (Temp[1] < 256 && Temp[2] < 256 && Temp[3] < 256) {
           return(Temp.slice(1, 4).concat(1));
         }
@@ -639,10 +660,11 @@ var SVGtoPDF = function(doc, svg, x, y, options) {
                 Text = Text.replace(/[\s]+/g, ' ');
                 if (AddedText.match(/[\s]$/)) {Text = Text.replace(/^[\s]/, '');}
               }
-              var MeasuredTextWidth = doc.widthOfString(Text);
+              var MeasuredTextWidth = doc.widthOfString(Text), ScaleX = 1;
               if (Anchor === 'end' || Anchor === 'middle' || Length) {
                 if (Length && Length !== MeasuredTextWidth) {
-                  doc.transform(Length / MeasuredTextWidth, 0, 0, 1, 0, 0)
+                  ScaleX = Length / MeasuredTextWidth;
+                  doc.transform(ScaleX, 0, 0, 1, 0, 0);
                 }
                 if (Anchor === 'end') {
                   CurrentTextX -= MeasuredTextWidth;
@@ -651,7 +673,7 @@ var SVGtoPDF = function(doc, svg, x, y, options) {
                 }
               }
               if (!Styles.invisible) {
-                doc._fragment(Text, CurrentTextX, CurrentTextY, {fill:true, stroke:!!StrokeOpacity, baseline: Baseline, oblique:FontStylesFound.italicFound===false});
+                doc._fragment(Text, CurrentTextX / ScaleX, CurrentTextY, {fill:true, stroke:!!StrokeOpacity, baseline: Baseline, oblique:FontStylesFound.italicFound===false});
               }
               CurrentTextX += Length || MeasuredTextWidth;
               AddedText += Text;
