acd820b982f1fe3f0b57ff02107a434b94e2ae2a
alafr <antlafranchis@gmail.com>
2016-11-20 21:03:43 +0200

    Fix issue with gradients inside a group

--- PDFGroup.js
+++ PDFGroup.js
@@ -1,54 +1,58 @@
 PDFDocument.prototype.addContent = function(data) {
   (this._writeTarget || this.page).write(data);
-  return(this);
+  return this;
 };
 PDFDocument.prototype.createGroup = function() {
-  var PDFGroup = function() {};
-  var group = new PDFGroup();
+  let group = new (function PDFGroup() {})();
   group.name = 'G' + (this._groupCount = (this._groupCount || 0) + 1);
   group.closed = false;
+  group.matrix = [1, 0, 0, 1, 0, 0];
   group.xobj = this.ref({
     Type: 'XObject', 
     Subtype: 'Form', 
     FormType: 1, 
-    BBox: [0, 0, this.page.width, this.page.height], 
+    BBox: [-1000000, -1000000, 1000000, 1000000], 
     Group: {S: 'Transparency', CS: 'DeviceRGB', I: true, K: false}
   });
   group.previousGroup = this._currentGroup;
   this.writeToGroup(group);
-  return(group);
+  return group;
 };
 PDFDocument.prototype.writeToGroup = function(group) {
-  if (group && !group.closed) {
-    this._currentGroup = group;
-    this._writeTarget = group.xobj;
+  let prevGroup = this._currentGroup,
+      nextGroup = group && (!group.closed) && group || null;
+  if (nextGroup) {
+    this._currentGroup = nextGroup;
+    this._writeTarget = nextGroup.xobj;
   } else {
     this._currentGroup = null;
     this._writeTarget = null;
   }
-  return(this);
+  if (prevGroup) {prevGroup.matrix = doc._ctm.slice();}
+  if (nextGroup) {doc._ctm = nextGroup.matrix.slice();}
+  return this;
 };
 PDFDocument.prototype.closeGroup = function(group) {
   this.page.xobjects[group.name] = group.xobj;
   group.xobj.end();
   group.closed = true;
   this.writeToGroup(group.previousGroup);
-  return(this);
+  return this;
 };
 PDFDocument.prototype.insertGroup = function(group) {
   if (!group.closed) {this.closeGroup(group);}
   this.addContent('/' + group.name + ' Do');
-  return(this);
+  return this;
 };
 PDFDocument.prototype.applyMask = function(group, clip) {
   if (!group.closed) {this.closeGroup(group);}
-  var name = 'M'+ (this._maskCount = (this._maskCount || 0) + 1);
-  var gstate = this.ref({
+  let name = 'M'+ (this._maskCount = (this._maskCount || 0) + 1);
+  let gstate = this.ref({
     Type: 'ExtGState', CA: 1, ca: 1, BM: 'Normal',
     SMask: {S: 'Luminosity', G: group.xobj, BC: (clip ? [0,0,0] : [1,1,1])}
   });
-  this.page.ext_gstates[name] = gstate;
   gstate.end();
+  this.page.ext_gstates[name] = gstate;
   this.addContent("/" + name + " gs");
-  return(this);
+  return this;
 };
